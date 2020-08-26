eb45c7431fd5675e4e69d68f2ef5c674e55f1f1e
Astemir <shibzuhov2000@mail.ru>
2020-08-26 21:42:36 +0300

    Initial Commit

---
+++ iMovie/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iMovie
+//
+//  Created by Astemir Shibzuhov on 26.08.2020.
+//  Copyright © 2020 Astemir Shibzuhov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
