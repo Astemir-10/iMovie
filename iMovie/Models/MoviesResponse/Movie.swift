a44a1482066609e12b1d94bf94c81792b3e3103e
Astemir <shibzuhov2000@mail.ru>
2020-08-28 02:40:07 +0300

    Changing Movie Model

--- iMovie/Models/MoviesResponse/Movie.swift
+++ iMovie/Models/MoviesResponse/Movie.swift
@@ -8,20 +8,35 @@
 
 import Foundation
 
+//"genre_ids": [
+//  28,
+//  80,
+//  878
+//],
+
 struct Movie {
   let title: String
   let posterUrl: String
+  let voteAvg: Double
+  let genres: [Int]
+  let dateRelease: String
 }
 
 extension Movie: Decodable {
   private enum MovieCodingKey: String, CodingKey {
     case title
     case posterUrl = "poster_path"
+    case voteAvg = "vote_average"
+    case genres = "genre_ids"
+    case dateRelease = "release_date"
   }
   
   init(from decoder: Decoder) throws {
     let container = try decoder.container(keyedBy: MovieCodingKey.self)
     title = try container.decode(String.self, forKey: .title)
     posterUrl = try container.decode(String.self, forKey: .posterUrl)
+    voteAvg = try container.decode(Double.self, forKey: .voteAvg)
+    genres = try container.decode([Int].self, forKey: .genres)
+    dateRelease = try container.decode(String.self, forKey: .dateRelease)
   }
 }
